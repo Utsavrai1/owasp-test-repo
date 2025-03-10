a09338b9ced7f9630bb9ba643c319d84ca4da31f
Utsav Rai <96744747+Utsavrai1@users.noreply.github.com>
2025-03-10 19:31:18 +0530

    Update serialization.py

--- serialization.py
+++ serialization.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/unpickle', methods=['POST'])
 def unpickle():
     data = request.form['data']
-    obj = pickle.loads(base64.b64decode(data))  # Vulnerable to insecure deserialization
+    obj = pickle.loads(base64.b64decode(data))
     return "Deserialized Object: " + str(obj)
 
 app.run(port=4000)
